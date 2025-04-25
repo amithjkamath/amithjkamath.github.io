34556afd7985b3c342e02a3bd5d93042f9a3027b
amithjkamath <amithjkamath@users.noreply.github.com>
2025-04-25 13:57:54 +0000

    Deploying to gh-pages from @ amithjkamath/amithjkamath.github.io@1b7934ced56e673a5c13d6570b7c78323d2df757 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -810,46 +810,6 @@ ninja.data = [{
           description: "More details about our MIDL &#39;24 work.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2024-midl-radonc-vs-dldp/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{id: "projects-qualitative-analysis-of-radiation-oncology-contour-corrections",
           title: 'Qualitative analysis of Radiation Oncology Contour Corrections',
           description: "Understanding what human-experts think about the impact of contour evaluations",
@@ -875,6 +835,11 @@ ninja.data = [{
           description: "A work-in-progress course plan for Radiation Oncology and AI",
           section: "Projects",handler: () => {
               window.location.href = "/projects/thesis-short-course-on-RadOnc-with-AI/";
+            },},{id: "projects-literature-review-for-a-business-plan-on-automated-contouring",
+          title: 'Literature review for a business plan on automated contouring',
+          description: "Broad overview of components of a business plan for automated contouring",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/translate-business-plan-contouraid/";
             },},{
         id: 'social-email',
         title: 'email',
@@ -904,18 +869,18 @@ ninja.data = [{
           window.open("https://orcid.org/0000-0002-7243-8883", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=clej42kAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-semanticscholar',
+        title: 'Semantic Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.semanticscholar.org/author/9083011", "_blank");
         },
       },{
         id: 'social-stackoverflow',
