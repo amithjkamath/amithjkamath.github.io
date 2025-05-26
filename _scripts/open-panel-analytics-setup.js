1a0c3dc4d173e290167ad07b3e6cdd09d2d9b0b6
AKA Math <amithjkamath@users.noreply.github.com>
2025-05-26 12:29:43 +0000

    making the .js file ES5 compatible

--- _scripts/open-panel-analytics-setup.js
+++ _scripts/open-panel-analytics-setup.js
@@ -1,11 +1,10 @@
 ---
 permalink: /assets/js/open-panel-analytics-setup.js
 ---
-window.op =
-  window.op ||
-  function (...args) {
-    (window.op.q = window.op.q || []).push(args);
-  };
+window.op = window.op || function () {
+  var args = Array.prototype.slice.call(arguments);
+  (window.op.q = window.op.q || []).push(args);
+};
 window.op("init", {
   clientId: "{{ site.openpanel_analytics }}",
   trackScreenViews: true,
